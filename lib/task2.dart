68b0286f1109197018bd17fc739a42fe5af62acd
abdalazeez1 <alazeez353@gmail.com>
2021-08-07 08:08:58 +0300

    ToDo

--- lib/task2.dart
+++ lib/task2.dart
@@ -21,60 +21,57 @@ class _TAsk2State extends State<TAsk2> {
     var size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        body: showResult
-            ? buildResuiltCard(size)
-            : buildBmiCal(size),
+        body: showResult ? buildResuiltCard(size) : buildBmiCal(size),
       ),
     );
   }
 
   GestureDetector buildResuiltCard(Size size) {
     return GestureDetector(
-              onTap: returnToHomeScreen,
-              child: Container(
-                color: Colors.grey.shade200,
-                height: size.height,
-                width: size.width,
-                child: AnimatedAlign(
-                  duration: Duration(milliseconds: 600),
-                  alignment:
-                      changeAlign ? Alignment.center : Alignment.bottomCenter,
-                  child: AnimatedContainer(
-                    duration: Duration(milliseconds: 600),
-                    padding: EdgeInsets.only(top: 30),
-                    height: changeAlign ? size.height * 0.3 : 20,
-                    width: changeAlign ? size.width * 0.5 : 20,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25),
-                        color: Colors.indigo.shade500),
-                    child: hideContent
-                        ? Column(
-                            children: [
-                              Text(
-                                'so ${resut>25?'wight':'thin'} ',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold),
-                                textAlign: TextAlign.center,
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                '${resut.toInt()}',
-                                style: TextStyle(
-                                    color: Colors.white70,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
-                              )
-                            ],
-                          )
-                        : Container(),
-                  ),
-                ),
-              ),
-            );
+      onTap: returnToHomeScreen,
+      child: Container(
+        color: Colors.grey.shade200,
+        height: size.height,
+        width: size.width,
+        child: AnimatedAlign(
+          duration: Duration(milliseconds: 600),
+          alignment: changeAlign ? Alignment.center : Alignment.bottomCenter,
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 600),
+            padding: EdgeInsets.only(top: 30),
+            height: changeAlign ? size.height * 0.3 : 20,
+            width: changeAlign ? size.width * 0.5 : 20,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.indigo.shade500),
+            child: hideContent
+                ? Column(
+                    children: [
+                      Text(
+                        'so ${resut > 25 ? 'wight' : 'thin'} ',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        '${resut.toInt()}',
+                        style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  )
+                : Container(),
+          ),
+        ),
+      ),
+    );
   }
 
   void returnToHomeScreen() {
@@ -105,11 +102,13 @@ class _TAsk2State extends State<TAsk2> {
             height: size.height * 0.1,
           ),
           Container(
-
               width: size.width * 2 / 3,
-              child: ElevatedButton(style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.indigo.shade400)
-              ),onPressed: cal, child: Text('calculate')))
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.indigo.shade400)),
+                  onPressed: cal,
+                  child: Text('calculate')))
         ],
       ),
     );
@@ -140,7 +139,7 @@ class _TAsk2State extends State<TAsk2> {
           ),
         ),
         Slider(
-           activeColor: Colors.indigo.shade300,
+            activeColor: Colors.indigo.shade300,
             min: isHeight ? 50 : 20,
             max: isHeight ? 200 : 150,
             value: isHeight ? valForHeight : valueForWight,
@@ -188,9 +187,10 @@ class _TAsk2State extends State<TAsk2> {
       hideContent = false;
     });
     Timer(Duration(milliseconds: 600), () {
-      setState(() {
+      setState((){
         changeAlign = true;
-        Timer(Duration(milliseconds: 300),(){
+        Timer
+          (Duration(milliseconds: 300), () {
           setState(() {
             hideContent = true;
           });
